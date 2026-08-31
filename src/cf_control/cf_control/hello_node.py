5da3fe15820aa38acedfff0afe483e18b025a133
Tanner Looney <tannerlooney@gmail.com>
2026-08-30 21:35:27 -0500

    Initial ROS 2 Crazyflie control package

---
+++ src/cf_control/cf_control/hello_node.py
@@ -0,0 +1,45 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class HelloNode(Node):
+
+    def __init__(self):
+        super().__init__('hello_node')
+
+        self.publisher_ = self.create_publisher(
+            String,
+            'cf_status',
+            10
+        )
+
+        self.timer = self.create_timer(
+            1.0,
+            self.publish_status
+        )
+
+    def publish_status(self):
+        msg = String()
+        msg.data = 'Crazyflie ROS node alive'
+
+        self.publisher_.publish(msg)
+
+        self.get_logger().info(
+            f'Publishing: "{msg.data}"'
+        )
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = HelloNode()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
